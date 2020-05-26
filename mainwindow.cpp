3890df6276b80075e53e467c916b7d83099349b0
chenghaopeng <i@chper.cn>
2020-05-26 12:49:28 +0800

    😒 尽量显示数据

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -31,7 +31,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     setStyleSheet(Utils::readFile(":/static/default.qss"));
     cpuDir = findCPUDir();
-    if (!cpuDir.isEmpty()) updateTimer = startTimer(500);
+    updateTimer = startTimer(500);
     cpuNumber = Utils::exec("cpufreq-info | grep 'analyzing CPU'").split("\n").size();
 }
 
@@ -54,20 +54,22 @@ void MainWindow::changeStrategy(QString strategy) {
 }
 
 void MainWindow::update() {
-    ui->lblCpuTemperature->setText(Utils::exec("cat " + cpuDir + "/" + temperature + " | cut -c-2") + "℃");
     ui->lblCpuFrequency->setText(Utils::exec("cat /proc/cpuinfo | grep -i mhz | awk 'BEGIN {sum=0; max=0; min=99999;} {if ($4 > max) max = $4; fi; if ($4 < min) min = $4; fi; sum += $4} END {printf(\"平均 %d 最大 %d 最小 %d\", int(sum / NR), max, min)}'"));
-    if (Utils::exec("cat " + cpuDir + "/" + speedMethod) == "1") {
-        ui->lblFanSpeed->setText("手动 " + Utils::exec("cat " + cpuDir + "/" + manualSpeed) + "/255");
-    }
-    else {
-        ui->lblFanSpeed->setText("自动 " + Utils::exec("cat " + cpuDir + "/" + currentSpeed));
-    }
     QString cm = Utils::exec("cpufreq-info -p | awk '{print($3)}'"), cmz;
     if (cm == ONDEMAND) cmz = "按需";
     else if (cm == PERFORMANCE) cmz = "性能";
     else if (cm == POWERSAVE) cmz = "节能";
     else cmz = "其他";
     ui->lblCpuStrategy->setText(cmz);
+    if (!cpuDir.isEmpty()) {
+        ui->lblCpuTemperature->setText(Utils::exec("cat " + cpuDir + "/" + temperature + " | cut -c-2") + "℃");
+        if (Utils::exec("cat " + cpuDir + "/" + speedMethod) == "1") {
+            ui->lblFanSpeed->setText("手动 " + Utils::exec("cat " + cpuDir + "/" + manualSpeed) + "/255");
+        }
+        else {
+            ui->lblFanSpeed->setText("自动 " + Utils::exec("cat " + cpuDir + "/" + currentSpeed));
+        }
+    }
 }
 
 MainWindow::~MainWindow()
@@ -77,11 +79,13 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btnFanSpeedAuto_clicked()
 {
+    if (cpuDir.isEmpty()) return;
     Utils::writeSystemFile(cpuDir + "/" + speedMethod, "2");
 }
 
 void MainWindow::on_sdrFanSpeed_sliderReleased()
 {
+    if (cpuDir.isEmpty()) return;
     Utils::writeSystemFile(cpuDir + "/" + manualSpeed, QString::number(ui->sdrFanSpeed->value()));
 }
 
